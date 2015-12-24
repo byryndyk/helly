4ba1bf2a7f25c87ca20a7a3188973136e2554e0f
lyolik <byryndyk@gmail.com>
2015-12-24 11:49:30 +0200

    new

--- php/head.php
+++ php/head.php
@@ -15,42 +15,42 @@
     <div id="line-top"></div>
     <div id="menu">
         <ul id="menu" class="main-menu">
-            <li id="menu-main"><a href="/helly/index.php">Main</a></li>
-            <li id="menu-main"><a href="/helly/php/work/work_list.php">Work</a>
+            <li id="menu-main"><a href="/helly/index.php">Главная</a></li>
+            <li id="menu-main"><a href="/helly/php/work/work_list.php">Работы</a>
                 <ul>
-                    <li><a href="/helly/php/work/hel_work.php">Helicopter</a></li>
-                    <li><a href="/helly/php/work/unit_work.php">Unit</a></li>
-                    <li><a href="/helly/php/work/people_work.php">People</a></li>
-                    <li><a href="/helly/php/work/wearhouse_work.php">Wearhouse</a></li>
+                    <li><a href="/helly/php/work/hel_work.php">Вертолеты</a></li>
+                    <li><a href="/helly/php/work/unit_work.php">Устройства</a></li>
+                    <li><a href="/helly/php/work/people_work.php">Люди</a></li>
+                    <li><a href="/helly/php/work/wearhouse_work.php">Склад</a></li>
                 </ul>
             </li>
 
-            <li id="menu-main"><a href="/helly/php/hel/hel_list.php">Helicopter</a>
+            <li id="menu-main"><a href="/helly/php/hel/hel_list.php">Вертолеты</a>
                 <ul>
-                    <li><a href="/helly/php/hel/hel_list.php">List</a></li>
-                    <li><a href="/helly/php/hel/hel_ed.php">Edit</a></li>
-                    <li><a href="/helly/php/hel/hel_add.php">Add</a></li>
-                    <li><a href="/helly/php/hel/hel_del.php">Delete</a></li>
+                    <li><a href="/helly/php/hel/hel_list.php">список</a></li>
+                    <li><a href="/helly/php/hel/hel_ed.php">Редактировать</a></li>
+                    <li><a href="/helly/php/hel/hel_add.php">Добавить</a></li>
+                    <li><a href="/helly/php/hel/hel_del.php">Удалить</a></li>
 	    		</ul>
             </li>
-            <li id="menu-main"><a href="/helly/php/unit/unit_list.php">Unit</a>
+            <li id="menu-main"><a href="/helly/php/unit/unit_list.php">Устройства</a>
                 <ul>
-                    <li><a href="/helly/php/unit/unit_list.php">List</a></li>
-                    <li><a href="/helly/php/unit/unit_ed.php">Edit</a></li>
-                    <li><a href="/helly/php/unit/unit_add.php">Add</a></li>
-                    <li><a href="/helly/php/unit/unit_del.php">Delete</a></li>
+                    <li><a href="/helly/php/unit/unit_list.php">Список</a></li>
+                    <li><a href="/helly/php/unit/unit_ed.php">Редактировать</a></li>
+                    <li><a href="/helly/php/unit/unit_add.php">Добаить</a></li>
+                    <li><a href="/helly/php/unit/unit_del.php">Удалить</a></li>
 	    		</ul>
 	    	</li>
             <li id="menu-main"><a href="/helly/php/people/people_list.php">People</a>
                 <ul>
-                    <li><a href="/helly/php/people/people_list.php">List</a></li>
-                    <li><a href="/helly/php/people/group_list.php">Group</a></li>
-                    <li><a href="/helly/php/people/people_ed.php">Edit</a></li>
-                    <li><a href="/helly/php/people/people_add.php">Add</a></li>
-                    <li><a href="/helly/php/people/people_del.php">Delete</a></li>
+                    <li><a href="/helly/php/people/people_list.php">Люди</a></li>
+                    <li><a href="/helly/php/people/group_list.php">Группа</a></li>
+                    <li><a href="/helly/php/people/people_ed.php">Редактировать</a></li>
+                    <li><a href="/helly/php/people/people_add.php">Добавить</a></li>
+                    <li><a href="/helly/php/people/people_del.php">Удалить</a></li>
 	    		</ul>
             </li>
-            <li id="menu-main"><a href="contact.php">Contacts</a></li>
+            <li id="menu-main"><a href="contact.php">Контакты</a></li>
         </ul>
     </div>
 	
