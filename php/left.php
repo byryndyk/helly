7a0b411fa6999188322a1aa7b7073f81acc70d5a
lyolik <byryndyk@gmail.com>
2015-12-24 11:59:36 +0200

    add left.php

---
+++ php/left.php
@@ -0,0 +1,20 @@
+<div id="left-frame">
+
+    <table id="plan">
+        <tr><th>Список вертолетов</th></tr>
+        <?php
+        $query = 'SELECT id, type, air_number FROM copter ORDER by TYPE';
+        $res = mysql_query($query) or die (mysql_error());
+        echo "<tr>";
+        while ($row = mysql_fetch_array($res)) {
+            echo "<tr>";
+            echo "<td><a href='/helly/php/hel/hel_list_one.php?id=" . $row["id"] . "'>" . $row['air_number'] . "</td>";
+            echo "</tr>";
+        }
+
+
+        
+        echo "</tr>";
+        ?>
+    </table>
+</div>
