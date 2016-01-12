4fa53c48a1a778020a7502081345882f3ab7a551
byryndyk <byryndyk@gmail.com>
2016-01-12 11:46:51 +0200

    test

--- php/right.php
+++ php/right.php
@@ -1,6 +1,6 @@
 <div id="right-frame">
     <table id="plan">
-        <tr><th>Работы</th></tr>
+        <tr><th>План Работ</th></tr>
         <tr><td>fsjldjlfsn</td></tr>
         <tr><td>fsjldjlfsn</td></tr>
         <tr><td>fsjldjlfsn</td></tr>
